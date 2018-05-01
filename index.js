8fafcb8f99634069a4a1af1af4106d842b57988a
Joaquín Fernández <jf@joaquinfernandez.net>
2018-05-01 15:26:52 +0200

    [src] Exportado el método render

--- index.js
+++ index.js
@@ -14,6 +14,12 @@ module.exports = {
             filename => this.registerHelper(hbs, filename)
         );
     },
+    /**
+     * Registra un helper en handlebars.
+     *
+     * @param {Handlebars} hbs      Módulo de handlebars a usar.
+     * @param {String}     filename Ruta completa del archivo del helper.
+     */
     registerHelper(hbs, filename)
     {
         hbs.registerHelper(
@@ -21,6 +27,45 @@ module.exports = {
             require(filename)
         );
     },
+    /**
+     * Compila una plantilla y devuelve el resultado.
+     *
+     * @param {Handlebars} hbs      Manejador de la plantilla.
+     * @param {String}     filename Ruta completa del archivo de la plantilla.
+     * @param {Object}     context  Contexto de la plantilla.
+     * @param {Object}     options  Opciones para compilar la plantilla.
+     *
+     * @return {String} Plantilla renderizada.
+     */
+    render(hbs, filename, context = {}, options = {})
+    {
+        let _content = '';
+        if (filename && fs.existsSync(filename))
+        {
+            const _tpl = fs.readFileSync(filename, 'utf8');
+            if (_tpl)
+            {
+                _content = hbs.compile(
+                    _tpl,
+                    Object.assign(
+                        {
+                            noEscape : true
+                        },
+                        options
+                    )
+                )(context);
+            }
+        }
+
+        return _content;
+    },
+    /**
+     * Escanea un directorio de manera no recursiva.
+     *
+     * @param {String} dir Ruta del directorio a escanear.
+     *
+     * @return {String[]} Ruta completa de los archivos encontrados.
+     */
     scandir(dir)
     {
         return fs.readdirSync(dir).map(file => path.join(dir, file));
